1129df47fd45c8d608d9d9ffe4287fd02133ce3f
Mykolas Milašius <mykolas.milasius@gmail.com>
2024-03-06 02:14:19 +0200

    Confirmation menu successfully created, shows at the start of the game, needs fixing

--- ImmunityTD/Assets/Scripts/QuitGame.cs
+++ ImmunityTD/Assets/Scripts/QuitGame.cs
@@ -7,13 +7,27 @@ public class QuitGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    [SerializeField] GameObject quitPanel; // Quit_Confirmation_Panel
+
+    public void OpenConfirmationMenu()
+    {
+
+        quitPanel.SetActive(true); 
+    }
+
+
+    public void CloseConfirmationMenu()
+    {
+        quitPanel.SetActive(false);
     }
 
     public void QuitApp()
