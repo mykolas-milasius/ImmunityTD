1f4e4a7dc3a8e792c7e54b3026c20721c0e7bae8
Mykolas Milašius <mykolas.milasius@gmail.com>
2024-03-29 12:25:15 +0200

    SCRUM-62, added AudioManager game object, AudioManager script, successfully plays audio files in StartScreen and Game scenes

---
+++ ImmunityTD/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Source")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("Audio Clip")]
+    public AudioClip background;
+    public AudioClip mainmenu;
+    public AudioClip buttonClick;
+
+    private void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (sceneName == "StartScreen")
+        {
+            musicSource.clip = mainmenu;
+        }
+        else
+        {
+            if (sceneName == "Game")
+            {
+                musicSource.clip = background;
+            }
+        }
+        musicSource.volume = 0.1f;
+        musicSource.Play();
+    }
+}
