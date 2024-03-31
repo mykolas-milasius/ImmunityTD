6f438f5352a3a428dc9fb6fb6a54ca0b4c0f8b34
Simas Bradaitis <simasbra@proton.me>
2024-03-31 17:15:54 +0300

    SCRUM-82 Taskbar
    ##Done:
    * Added kills and score which are updated when enemy is killed
    #Aditional notes:
    * UI layering needs fixing

--- ImmunityTD/Assets/Scripts/Player.cs
+++ ImmunityTD/Assets/Scripts/Player.cs
@@ -6,13 +6,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public static float coins = 100f;
-    public TextMeshProUGUI coinsText;
     public static int score = 0;
     public static int kills = 0;
+    public TextMeshProUGUI coinsText;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI killsText;
 
     public void FixedUpdate()
     {
         coinsText.text = coins.ToString();
+        scoreText.text = score.ToString();
+        killsText.text = kills.ToString();
     }
 
     public static void AddCoins(float coinsToAdd){
