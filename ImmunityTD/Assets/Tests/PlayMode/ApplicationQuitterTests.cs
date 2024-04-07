f63c912dc6f64d4087ae357aad9606920b86e298
Mykolas Milašius <mykolas.milasius@gmail.com>
2024-04-07 21:27:04 +0300

    ApplicationQuitterTest added

---
+++ ImmunityTD/Assets/Tests/PlayMode/ApplicationQuitterTests.cs
@@ -0,0 +1,47 @@
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections;
+
+public class ApplicationQuitterTests
+{
+    private IApplicationQuitter quitter;
+
+    [SetUp]
+    public void SetUp()
+    {
+        quitter = new ApplicationQuitter();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        quitter = null;
+    }
+
+    [UnityTest]
+    public IEnumerator QuitApplication()
+    {
+        quitter.Quit();
+
+        yield return new WaitForSeconds(0.1f);
+
+        Assert.IsTrue(ApplicationQuitChecker.IsQuitting);
+    }
+
+    private static class ApplicationQuitChecker
+    {
+        private static bool isQuitting = false;
+
+        public static bool IsQuitting
+        {
+            get { return isQuitting; }
+        }
+
+        [RuntimeInitializeOnLoadMethod]
+        static void OnApplicationQuit()
+        {
+            isQuitting = true;
+        }
+    }
+}
